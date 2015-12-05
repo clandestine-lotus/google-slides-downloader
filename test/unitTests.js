453391e414386cfae71f2a4232176b227fb74ffd
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-12-04 22:13:49 -0800

    working on tests

--- test/unitTests.js
+++ test/unitTests.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var should = require('chai').should();
 var googleSlidesDownloader = require('../index.js');
 
@@ -5,6 +6,18 @@ describe('the google slides downloader', function () {
 
     it('should have a get method', function (){
       googleSlidesDownloader.get.should.be.a('function');
+    });
+
+    it('should return an array of SVGs from the attached raw Google html', function () {
+      fs.readFile('./google-raw.html', 'utf8', function (err, doc) {
+        var result = googleSlidesDownloader.get(doc);
+        result.should.be.instanceof(Array);
+        result.should.have.a.length.of(10);
+        result[0].should.be.a('string');
+        var startString = result[0].substr(0, 4);
+        console.log(startString);
+        result[0].substr(0, 4).should.equal('<s>');
+      });
     });
 
 });
